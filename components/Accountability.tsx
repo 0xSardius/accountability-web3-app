60cfa5d656ef56ddaa012811e5c8d4c6cb13315e
Sardius <0xsardius@gmail.com>
2024-06-28 23:03:43 -0400

    Accountability component update

--- components/Accountability.tsx
+++ components/Accountability.tsx
@@ -7,16 +7,36 @@ import {
 } from "thirdweb/react";
 import { chain } from "@/app/chain";
 import { client } from "@/app/client";
+import { contract } from "../utils/contract";
+import { deposit } from "thirdweb/extensions/erc20";
 
 export const Accountability = () => {
   const account = useActiveAccount();
 
-  const { data: depositAmount } = useReadContract();
+  const { data: depositAmount } = useReadContract(
+    contract: contract,
+    method: "getDeposit"
+  );
+
+  const {data: taskCount} = useReadContract({
+    contract: contract,
+    method: "getTaskCount",
+  });
+  }
 
   if (account) {
     return (
       <div style={{ textAlign: "center", minWidth: "500px" }}>
         <ConnectButton client={client} chain={chain} />
+        <div>
+          {depositAmount?.toString() == 0 && taskCount?.toString() == 0 ? (
+            <></>
+          ) : depositAmount?.toString() !== "0" && taskCount?.toString() === "0" ? (
+            <></>
+          ) : (
+            <></>
+          )}
+        </div>
       </div>
     );
   }
--- utils/contract.ts
+++ utils/contract.ts
@@ -1,7 +1,7 @@
 import { getContract } from "thirdweb";
 import { client } from "@/app/client";
 import { chain } from "@/app/chain";
-import { contractABI } from "./contractABI.ts";
+import { contractABI } from "./contractABI";
 
 const contractAddress = "0xF1a554439ea1B97476F3d187Aec3C636b319E86B";
 
